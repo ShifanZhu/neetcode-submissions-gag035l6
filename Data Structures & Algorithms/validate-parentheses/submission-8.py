5fd0ca96083f9ed54d65926c3ac6579bdc7235eb
ShifanZhu <84172927+ShifanZhu@users.noreply.github.com>
2026-04-06 14:12:39 -0400

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,42 @@
+# import queue
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Solution 1, works but not clean
+        # seen = deque()
+        # valid = False
+        # for c in s:
+        #     if c in '[({':
+        #         seen.append(c)
+        #     elif c in '])}':
+        #         if not seen:
+        #             return False
+        #         if c == ']':
+        #             valid = (seen.pop() == '[')
+        #             if not valid:
+        #                 return False
+        #         if c == ')':
+        #             valid = (seen.pop() == '(')
+        #             if not valid:
+        #                 return False
+        #         if c == '}':
+        #             valid = (seen.pop() == '{')
+        #             if not valid:
+        #                 return False
+        # return len(seen) == 0
+
+        # Solution 2
+        seen = deque()
+        pair = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+        for c in s:
+            if c in pair.values():
+                seen.append(c)
+            else: # keys
+              if pair[c] != seen.pop() and not seen:
+                return False
+        return len(seen) == 0
